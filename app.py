5bcc91c988d03657d70e037030e642246c4c8eec
Robert Morain <robert.morain@gmail.com>
2024-05-29 17:06:15 -0600

    Modified gameplay

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import pudb
 
 # from codenames.cnai import Spymaster, W2VAssoc, W2VGuesser
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 app.secret_key = "859c86bf1895e69b3c6dfc1c6092a3b3c45d9b55f22ac29aa816ed87793c00b8"
 socketio = SocketIO(app)
+
+players = {}
 
 
 @app.route("/")
@@ -31,7 +33,7 @@ def index():
 
 
 # Create new game
-@app.get("/create")
+@app.route("/create")
 def create_game():
     app.config["game_state"] = {
         "colors": [
@@ -117,28 +119,95 @@ def create_game():
         ],
         "current_turn": "blue",
         "current_clue": {"word": "Red", "number": 1},
-        "guesses_remaining": 0,
+        "guesses_remaining": 1,
     }
-    game()
-    return redirect(url_for("game"))
+
+    # Get the role and team parameters from the request
+    role = request.args.get("role")
+    team = request.args.get("team")
+
+    # Construct the URL with the query parameters
+    game_url = url_for("game", role=role, team=team)
+    return redirect(game_url)
 
 
 @app.route("/game")
 def game():
     if "game_state" not in app.config:
         create_game()
-    return render_template("game.html", game_state=app.config["game_state"])
+    # Assign a role and team to the player
+    role = request.args.get("role")
+    team = request.args.get("team")
+    player_id = session.get(
+        "player_id", request.remote_addr
+    )  # Use session or IP for simplicity
+    players[player_id] = {"team": team, "role": role}
+    print(role, team)
+    return render_template(
+        "game.html",
+        game_state=app.config["game_state"],
+        player_role=role,
+        player_team=team,
+    )
 
 
 @app.post("/update_game_state")
 def update_game_state():
-    updated_state = request.get_json()
+    data = request.get_json()
+    pu.db
+    if "gameState" in data.keys():
+        updated_state = data["gameState"]
+        current_guess = data["currentGuess"]
+        # Check if game is over
+        if game_over(updated_state, current_guess):
+            # TODO
+            # End game
+            pass
+        # Check if we need to change turn
+        if change_turn(updated_state, current_guess):
+            if updated_state["current_turn"] == "blue":
+                updated_state["current_turn"] = "red"
+            else:
+                updated_state["current_turn"] = "blue"
+    else:
+        updated_state = data
+
     app.config["game_state"].update(updated_state)
 
     socketio.emit("update", app.config["game_state"])
 
     # Return the updated game state back to the client
     return jsonify(app.config["game_state"])
+
+
+def change_turn(game_state, current_guess):
+    if (game_state["guesses_remaining"] == 0) or (guessed_wrong(current_guess)):
+        return True
+    else:
+        return False
+
+
+def guessed_wrong(current_guess):
+    game_state_colors = app.config["game_state"]["colors"]
+    current_turn = app.config["game_state"]["current_turn"]
+    if current_turn == "blue" and game_state_colors[current_guess] == "R":
+        return True
+    elif current_turn == "red" and game_state_colors[current_guess] == "U":
+        return True
+    else:
+        return False
+
+
+def game_over(updated_state, current_guess):
+    # TODO
+    pass
+
+
+@socketio.on("connect")
+def handle_connect():
+    player_id = session.get("player_id", request.remote_addr)
+    if player_id in players:
+        emit("player_info", players[player_id])
 
 
 if __name__ == "__main__":
--- static/js/game.js
+++ static/js/game.js
@@ -33,27 +33,45 @@ document.addEventListener('DOMContentLoaded', function() {
         const clueWord = gameState.current_clue.word;
         const clueNumber = gameState.current_clue.number;
         currentClueElement.textContent = `Current Clue: ${clueWord} (${clueNumber})`;
+
+        const guessesRemainingElement = document.getElementById('guesses-remaining');
+        guessesRemainingElement.textContent = `Guesses remaining: ${gameState.guesses_remaining}`;
+
+        const teamRoleElement = document.getElementById('team-role');
+        teamRoleElement.textContent = `Team: ${playerTeam} Role: ${playerRole}`;
+
     }
 
     function handleCardClick(event) {
             const cardElement = event.target;
             const index = parseInt(cardElement.getAttribute('data-index'), 10);
 
-            if (gameState.current_clue.number === 0) {
-                alert("No more guesses allowed this turn.");
-                return;
-            }
 
             if (!gameState.guessed[index]) {
+                const currentGuess = index;
+                if (gameState.current_turn != playerTeam){
+                    alert("Not your turn!");
+                    return;
+                }
+
+                if (playerRole != "guesser"){
+                    alert("You are not the guesser!");
+                    return;
+                }
+
+                if (gameState.guesses_remaining === 0) {
+                    alert("No more guesses allowed this turn.");
+                    return;
+                }
                 gameState.guessed[index] = true;
-                gameState.current_clue.number--; 
+                gameState.guesses_remaining--; 
 
                 fetch('/update_game_state', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify(gameState)
+                    body: JSON.stringify({gameState, currentGuess})
                 })
                 .then(response => response.json())
                 .then(data => {
@@ -62,11 +80,56 @@ document.addEventListener('DOMContentLoaded', function() {
                 })
                 .catch(error => console.error('Error:', error));
             }
+    }
+
+    // Function to handle clue submission
+    function handleClueSubmission() {
+        if (gameState.current_turn != playerTeam){
+            alert("Not your turn!");
+            return;
         }
+
+        if (playerRole != "spymaster"){
+            alert("You are not the spymaster!");
+            return;
+        }
+        const clueWordInput = document.getElementById('clue-word');
+        const clueNumberInput = document.getElementById('clue-number');
+
+        const newClueWord = clueWordInput.value.trim();
+        const newClueNumber = parseInt(clueNumberInput.value, 10);
+
+        if (newClueWord && !isNaN(newClueNumber)) {
+            gameState.current_clue.word = newClueWord;
+            gameState.current_clue.number = newClueNumber;
+            gameState.guesses_remaining = newClueNumber;
+
+            // Clear the input fields
+            clueWordInput.value = '';
+            clueNumberInput.value = '';
+            fetch('/update_game_state', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(gameState)
+            })
+            .then(response => response.json())
+            .then(data => {
+                gameState = data;
+                renderGameBoard();
+            })
+            .catch(error => console.error('Error:', error));
+        }
+
+    }
 
     const socket = io()
     socket.on('connect', () => {
             console.log('Connected to the server');
+        });
+    socket.on('player_info', (data) => {
+            console.log('Player Info:', data);
         });
 
     socket.on('update', (data) => {
@@ -77,5 +140,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const gameBoardElement = document.getElementById('game-board');
     gameBoardElement.addEventListener('click', handleCardClick);
 
+    // Add event listener for the "Submit Clue" button
+    if (playerRole == "spymaster"){
+        const submitClueButton = document.getElementById('submit-clue');
+        submitClueButton.addEventListener('click', handleClueSubmission);
+    }
     renderGameBoard();
 });
